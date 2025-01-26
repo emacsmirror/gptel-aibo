b658f1ea2bec004e6e976c5b49bd3c88ff5d7219
Sun Yi Ming <sam@SundeMacBook-Pro.local>
2025-01-26 12:05:57 +0800

    refine the complete at point

--- aics.el
+++ aics.el
@@ -256,8 +256,42 @@ If no suitable content can be suggested, return an empty string.")
   (interactive)
   (let ((gptel--system-message aics--system-role)
         (prompt (concat (aics-context--info) aics--complete-message)))
-    (message prompt)
-    (gptel-request prompt)))
+    ;; (message prompt)
+    (gptel-request prompt :callback #'aics--insert-response)))
+
+(defun aics--insert-response (response info)
+  "Insert the LLM RESPONSE into the calling buffer.
+
+INFO is a plist containing information relevant to this buffer.
+See `gptel--url-get-response' for details."
+  (let* ((gptel-buffer (plist-get info :buffer))
+         (start-marker (plist-get info :position))
+         (tracking-marker (plist-get info :tracking-marker)))
+    (cond
+     ((stringp response)                ;Response text
+      (with-current-buffer gptel-buffer
+        (when-let* ((transformer (plist-get info :transformer)))
+          (setq response (funcall transformer response)))
+        (when tracking-marker           ;separate from previous response
+          (setq response (concat "\n\n" response)))
+        (save-excursion
+          (add-text-properties
+           0 (length response) '(gptel response front-sticky (gptel)) response)
+          (with-current-buffer (marker-buffer start-marker)
+            (goto-char (or tracking-marker start-marker))
+            ;; (run-hooks 'gptel-pre-response-hook)
+            (unless (or (bobp) (plist-get info :in-place)
+                        tracking-marker)
+              ;; (insert "\n\n")
+              (when gptel-mode
+                (insert (gptel-response-prefix-string)))
+              (move-marker start-marker (point)))
+            (insert response)
+            (plist-put info :tracking-marker (setq tracking-marker (point-marker)))
+            ;; for uniformity with streaming responses
+            (set-marker-insertion-type tracking-marker t)))))
+     ((consp response)                  ;tool call or tool result?
+      (message "The LLM did not respond in the requested format.")))))
 
 (provide 'aics)
 ;;; aics.el ends here
