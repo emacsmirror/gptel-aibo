19636f673d664cd627c4865166438cb715548048
Sun Yi Ming <sam@SundeMacBook-Pro.local>
2025-02-06 21:48:39 +0800

    GPL-3.0 license

--- gptel-aibo.el
+++ gptel-aibo.el
@@ -10,16 +10,29 @@
 ;; Keywords: emacs tools editing gptel ai assistant code-completion productivity
 ;; Homepage: https://github.com/dolmens/gptel-aibo
 ;; Package-Requires: ((emacs "27.1") (gptel "0.9.0"))
-;;
-;; This file is not part of GNU Emacs.
-;;
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
-;;
+
 ;;  gptel-aibo is an AI-powered writing assistant that helps users create and
 ;;  manage content in Emacs, including programs, documents, papers, and novels.
 ;;  It provides various operations like buffer modifications, file creation and
 ;;  deletion.
-;;
+
 ;;; Code:
 
 (defvar gptai--system-role
